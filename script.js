ca29a11f9f13247a8754473fc9932595d665099b
Likhita Aluri <aluri_likhita@columbusstate.edu>
2020-11-01 03:07:45 +0000

    got the form in and all other portions

---
+++ script.js
@@ -0,0 +1,14 @@
+function validateForm(inputtxt) 
+{ 
+var passw=  /^[A-Za-z]\w{7,}/;
+if(inputtxt.value.match(passw)) 
+{ 
+alert('Correct, try another...')
+return true;
+}
+else
+{ 
+alert('Wrong...!')
+return false;
+}
+}
